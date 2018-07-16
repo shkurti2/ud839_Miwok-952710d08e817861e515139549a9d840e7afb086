55b8893a0971b963d4a91e5c8e78e79bc09faa64
shkurti2 <shkurtielton@gmail.com>
2018-07-16 14:58:39 +0200

    Initial commit

---
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.android.miwok;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class ColorsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.word_list);
+        // Create arraylist of words
+
+        ArrayList<Word> words = new ArrayList<Word>();
+
+        words.add(new Word("red", "weṭeṭṭi"));
+        words.add(new Word("mustard yellow", "chiwiiṭә"));
+        words.add(new Word("dusty yellow", "ṭopiisә"));
+        words.add(new Word("green", "chokokki"));
+        words.add(new Word("brown", "ṭakaakki"));
+        words.add(new Word("gray", "ṭopoppi"));
+        words.add(new Word("black", "kululli"));
+        words.add(new Word("white", "kelelli"));
+
+        // Find the root view so we can add child views to it
+        WordAdapter adapter = new WordAdapter(this , words);
+
+        // Create a new TextView
+        ListView listView = (ListView) findViewById(R.id.list);
+
+
+        // Set the text to be word at the current index
+        listView.setAdapter(adapter);
+
+    }
+}
+
